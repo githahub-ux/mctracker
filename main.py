f7b3e696834a710fec9247fbee8a7c90ad2797bc
Guthib User <tim.kaeppler12@gmail.com>
2026-06-04 10:14:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from typing import Any
 from urllib.parse import parse_qs, urlparse
 
 
-HOST = "127.0.0.1"
+HOST = "0.0.0.0"
 PORT = 8000
 DEFAULT_MC_HOST = "donutsmp.net"
 DEFAULT_MC_PORT = 25565
